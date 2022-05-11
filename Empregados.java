16fc0e1ea14bd920bfabe5ea727f1e00d06c8235
lipe1o <99926862+lipe1o@users.noreply.github.com>
2022-05-11 12:50:32 -0300

    Add files via upload

---
+++ Empregados.java
@@ -0,0 +1,18 @@
+package com.mycompany.empregados;
+
+import classes.Gerentes;
+
+public class Empregados {
+
+    public static void main(String[] args) {
+        Gerentes Felipe=new Gerentes();
+        Felipe.setNome("Felipe Castro da Silva");
+        Felipe.setCPF("000.000.000-00");
+        Felipe.setSalario(2000);
+        Felipe.setDepartamento("vendas");
+        Felipe.setHorario("manhã/tarde");
+        Felipe.setLogin("batata");
+        Felipe.setSenha("broto");
+        Felipe.setRamal("1231412");      
+    }    
+}
